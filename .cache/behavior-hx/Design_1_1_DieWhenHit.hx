030e7dedebc417830fdd4a36f1378ecfb1ad9765
ScottyO19 <scottoneil757@gmail.com>
2019-09-10 00:56:27 -0400

    Update of menus

--- .cache/behavior-hx/Design_1_1_DieWhenHit.hx
+++ .cache/behavior-hx/Design_1_1_DieWhenHit.hx
@@ -75,6 +75,18 @@ class Design_1_1_DieWhenHit extends ActorScript
 	override public function init()
 	{
 		
+		/* ======================== Something Else ======================== */
+		addCollisionListener(actor, function(event:Collision, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled)
+			{
+				/* See 'Explode on Death' behavior to see the logic for HandleDeath. */
+				actor.shout("_customEvent_" + "HandleDeath");
+				recycleActor(actor.getLastCollidedActor());
+				recycleActor(actor);
+			}
+		});
+		
 	}
 	
 	override public function forwardMessage(msg:String)
